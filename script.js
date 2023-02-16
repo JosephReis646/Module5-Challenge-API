4ebef3e24d446f5785df3dec7b8839bd9887db33
joseph reis <josephreis646@gmail.com>
2023-02-16 18:24:30 -0500

    Criteria 3:WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future

--- script.js
+++ script.js
@@ -29,4 +29,25 @@ $("#currentDay").text(currentDay);
 function updateTimeBlocks() {
   // get the current hour using Day.js
   var currentHour = dayjs().hour();
+
+
+ // loop through each time-block div
+ $(".time-block").each(function () {
+  // get the hour from the div's id attribute
+  var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  // check if the block hour is in the past, present, or future
+  if (blockHour < currentHour) {
+    $(this).removeClass("present future").addClass("past");
+  } else if (blockHour === currentHour) {
+    $(this).removeClass("past future").addClass("present");
+  } else {
+    $(this).removeClass("past present").addClass("future");
+  }
+});
 }
+
+// call updateTimeBlocks to update the colors on page load
+updateTimeBlocks();
+
+// update the colors every hour
+setInterval(updateTimeBlocks, 60 * 60 * 1000);
